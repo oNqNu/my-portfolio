4609f382c9246aa6f5105f9201614f1d9269661c
yuma-code <onanukintaro@gmail.com>
2021-12-27 17:32:50 +0900

    change-font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,26 +17,39 @@ export default function Home() {
   return (
     <>
       <Header />
-      <chakra.main>
+      <chakra.main fontFamily='Century Gothic'>
         <Flex>
-          <Box h='500' w='full' bg='gray.200' px='50'>
-            <Text mx='30' mt='70' fontSize='4xl' fontWeight='extrabold'>
-              About
-            </Text>
-            <Stack w='full' spacing={'5'} fontSize='lg'>
-              <Text mt='30'>こんにちは，山﨑 裕真です．</Text>
-              <Text>岡山大学自然科学研究科に所属し，</Text>
-              <Text>機械学習を用いた画像分類の研究をしています．</Text>
-              <Text>また，webアプリケーションの開発をしています．</Text>
-              <Text>たくさん食べて，たくさん体を動かすことが大好きです．</Text>
-            </Stack>
+          <Box w='full' bg='gray.200'>
+            <Box ml='100'>
+              <Text mt='70' fontSize='4xl' fontWeight='extrabold'>
+                About
+              </Text>
+              <Stack mb='50' w='full' spacing='5' fontSize='lg'>
+                <Text mt='30'>こんにちは，山﨑 裕真です．</Text>
+                <Text>愛媛県松山市生まれです．</Text>
+                <Text>岡山大学自然科学研究科に所属し，</Text>
+                <Text>機械学習を用いた画像分類の研究をしています．</Text>
+                <Text>また，webアプリケーションの開発をしています．</Text>
+                <Text>
+                  たくさん食べて，たくさん体を動かすことが大好きです．
+                </Text>
+              </Stack>
+            </Box>
           </Box>
-          <Box h='xl' w='full' bg='white'>
-            <Center h='full'>
-              <Image src='/my1.png' width={500} height={500} alt='いらすと' />
+          <Box w='full' bg='white'>
+            <Center>
+              <Image src='/my1.png' width={400} height={400} alt='いらすと' />
             </Center>
           </Box>
         </Flex>
+        <Box h='6xl' bg='blue.100' pt='70'>
+          <Text mx='100' fontSize='4xl' fontWeight='extrabold'>
+            Skills
+          </Text>
+          <Text mx='100' mt='30' fontSize='xl' fontWeight='extrabold'>
+            大学院で機械学習を用いた画像処理・分類の研究，また受託でWebアプリケーションの開発を行っています．
+          </Text>
+        </Box>
       </chakra.main>
     </>
   )
